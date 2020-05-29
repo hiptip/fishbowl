60ccb64975d80cc6a1f1e1c520eafc05557bba79
Bick Noston <bicknoston@Annas-MBP-221.fios-router.home>
2020-05-28 20:41:07 -0400

    misc

--- fishbowl-react/client/src/FishBowl/NewSession.js
+++ fishbowl-react/client/src/FishBowl/NewSession.js
@@ -13,30 +13,13 @@ class NewSession extends React.Component {
         this.props.socket.emit('hostCreateNewGame');
     }
 
+    joinSession = () => {
+        
+    }
+
     gameDeets = (data) => {
         console.log(data)
     }
-
-
-    // init() {
-    //     IO.socket = io.connect();
-    //     IO.bindEvents();
-    // },
-
-    // /**
-    //  * While connected, Socket.IO will listen to the following events emitted
-    //  * by the Socket.IO server, then run the appropriate function.
-    //  */
-    // bindEvents : function() {
-    //     IO.socket.on('connected', IO.onConnected );
-    //     IO.socket.on('newGameCreated', IO.onNewGameCreated );
-    //     IO.socket.on('playerJoinedRoom', IO.playerJoinedRoom );
-    //     IO.socket.on('beginNewGame', IO.beginNewGame );
-    //     IO.socket.on('newWordData', IO.onNewWordData);
-    //     IO.socket.on('hostCheckAnswer', IO.hostCheckAnswer);
-    //     IO.socket.on('gameOver', IO.gameOver);
-    //     IO.socket.on('error', IO.error );
-    // },
 
     render() {
         return (
