7a480d01843f2365e87bbe014261b9de77216d9d
Bick Noston <bicknoston@Annas-MBP-221.fios-router.home>
2020-05-28 22:39:08 -0400

    simple join page

--- fishbowl-react/client/src/FishBowl/JoinSession.js
+++ fishbowl-react/client/src/FishBowl/JoinSession.js
@@ -2,10 +2,37 @@ import React from 'react';
 import SocketContext from '../SocketContext'
 
 class JoinSession extends React.Component {
+    constructor(props) {
+        super(props);
+        this.playerName = React.createRef();
+        this.gameID = React.createRef();
+    }
+
+
+    joinGame = () => {
+        var data = {
+            gameId : this.gameID.current.value,
+            playerName : this.playerName.current.value || 'anon'
+        };
+        console.log(data);
+        // Send the gameId and playerName to the server
+        this.props.socket.emit('playerJoinGame', data);
+
+        // Set the appropriate properties for the current player.
+        // App.myRole = 'Player';
+        // App.Player.myName = data.playerName;
+    }
+
     render() {
         return (
             <div className='Join-session'>
-                <input></input>
+                <div>
+                    <label>Name:</label>
+                    <input ref={this.playerName}></input>
+                    <label >Room code:</label>
+                    <input ref={this.gameID}></input>
+                    <button onClick={this.joinGame}>Enter</button>
+                </div>
             </div>
         )
     }
